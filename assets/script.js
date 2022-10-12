e2bd38bffdf5eae29eb8c5bb82d0ed28c760e7e7
Leone Varando <106971202+Opaleone@users.noreply.github.com>
2022-10-11 23:58:51 -0500

    Added search history buttons

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 var searchBtn = document.querySelector('#search-btn');
 
-let searchInput = document.querySelector('#searchbar').value;
+let searchBar = document.querySelector('#searchbar');
+
+let searchHistory = document.querySelector('#city-history');
 
 var currentCity = document.querySelector('#city-title');
 var currentDate = document.querySelector('#date-today');
@@ -9,7 +11,25 @@ var currentTemp = document.querySelector('#current-temp');
 var currentWind = document.querySelector('#current-wind');
 var currentHumidity = document.querySelector('#current-humidity');
 
+var cityName;
 
+var count = 0;
+
+// Local Storage Variable
+
+var historyOfScores = JSON.parse(localStorage.getItem('historyOfScores'));
+
+if (historyOfScores === null) {
+  historyOfScores = [];
+
+  localStorage.setItem('historyOfScores', JSON.stringify(historyOfScores));
+}
+
+searchHistoryButtonsOnRefresh();
+
+console.log(historyOfScores);
+
+// id='search-btn-history'
 // var requestUrl = 'https://api.openweathermap.org/data/3.0/onecall?lat=' + lat + '&lon=' + long + '&units=imperial&appid=1ac2e1ab97a3d396986d8b6bd6516f63';
 // var latLong = 'https://api.openweathermap.org/geo/1.0/direct?q=Austin&limit=1&appid=1ac2e1ab97a3d396986d8b6bd6516f63';
 // https://api.openweathermap.org/data/3.0/onecall?lat=30.266666&lon=-97.733330&units=imperial&appid=1ac2e1ab97a3d396986d8b6bd6516f63;
@@ -31,6 +51,35 @@ var dailyWeather = {
   unixMeasures: [],
   icons: []
 };
+
+
+function searchHistoryButtonsOnRefresh() {
+  for (let i = 0; i < historyOfScores.length; i++) {
+    var historyBtn = document.createElement('button');
+
+    historyBtn.setAttribute('id', 'search-btn-history')
+    historyBtn.setAttribute('value', historyOfScores[i]);
+
+    historyBtn.textContent = historyOfScores[i];
+
+    searchHistory.appendChild(historyBtn);
+
+  }
+  
+}
+
+function searchHistoryButtonsCreation(cityName) {
+
+  var historyBtn = document.createElement('button');
+
+  historyBtn.setAttribute('id', 'search-btn-history')
+  historyBtn.setAttribute('value', cityName);
+
+  historyBtn.textContent = cityName;
+
+  searchHistory.appendChild(historyBtn);
+  
+}
 
 
 function getLatLong(searchInput) {
@@ -68,6 +117,23 @@ function getWeather(lat, long) {
     .then (function (data) {
       console.log(data);
 
+      currentWeather = {
+        temp: '',
+        humidity: '',
+        windSpeed: '',
+        unixMeasure: '',
+        icon: ''
+      };
+      
+      dailyWeather = {
+        temps: [],
+        humidities: [],
+        windSpeeds: [],
+        unixMeasures: [],
+        icons: []
+      };
+      // if ()
+
       currentWeather.temp = data.current.temp;
       currentWeather.humidity = data.current.humidity;
       currentWeather.windSpeed = data.current.wind_speed;
@@ -85,15 +151,13 @@ function getWeather(lat, long) {
       displayCurrentWeather();
       displayDailyForecast();
 
-      
-
       document.querySelector('#info-display').style.display = 'flex';
     })
 }
 
 
 function displayCurrentWeather() {
-  currentCity.textContent = `${searchInput} (${currentWeather.unixMeasure})`
+  currentCity.textContent = `${cityName} (${currentWeather.unixMeasure})`
   currentIcon.src = `http://openweathermap.org/img/wn/${currentWeather.icon}@2x.png`
   currentTemp.textContent = `Temp: ${currentWeather.temp}°F`
   currentWind.textContent = `Wind: ${currentWeather.windSpeed} MPH`
@@ -102,7 +166,9 @@ function displayCurrentWeather() {
 
 function displayDailyForecast() {
 
+  let targetEl = document.createElement('div');
 
+  count++;
   
   for (let i = 1; i < 6; i++) {
 
@@ -114,6 +180,8 @@ function displayDailyForecast() {
     let pEl1 = document.createElement('p')
     let pEl2 = document.createElement('p')
     let pEl3 = document.createElement('p')
+
+    targetEl.setAttribute('id', 'target-container');
 
     divEl.setAttribute('class', 'forecast-card');
 
@@ -129,7 +197,8 @@ function displayDailyForecast() {
     pEl2.textContent = `Wind: ${dailyWeather.windSpeeds[i]} MPH`
     pEl3.textContent = `Humidity: ${dailyWeather.humidities[i]}%`
 
-    forecastSection.appendChild(divEl);
+    forecastSection.appendChild(targetEl);
+    targetEl.appendChild(divEl);
     divEl.appendChild(h4El);
     divEl.appendChild(imgEl);
     divEl.appendChild(pEl1);
@@ -138,12 +207,85 @@ function displayDailyForecast() {
   }
 }
 
-// function searchHistory() {
 
-// }
 
+// Event listener for search button click
 searchBtn.addEventListener('click', function () {
+  document.querySelector('#info-display').style.display = 'none';
+
   let searchInput = document.querySelector('#searchbar').value;
 
-  getLatLong(searchInput);
+  if (count > 0) {
+    document.querySelector('#target-container').remove()
+  }
+
+  cityName = searchInput;
+
+  searchHistoryButtons();
+
+  if (historyBtn.includes(cityName)) {
+    getLatLong(searchInput);
+  } else {
+    historyOfScores.push(cityName);
+    localStorage.setItem('historyOfScores', JSON.stringify(historyOfScores));
+
+    searchHistoryButtonsCreation(cityName);
+
+    getLatLong(searchInput);
+  }
+
 })
+
+// Event listener for Enter key on Search Bar
+searchBar.addEventListener('keyup', function (event) {
+  if (event.key !== 'Enter') {
+    return;
+  }
+
+  document.querySelector('#info-display').style.display = 'none';
+
+  if (count > 0) {
+    document.querySelector('#target-container').remove()
+  }
+
+  let searchInput = document.querySelector('#searchbar').value;
+
+  cityName = searchInput;
+
+  if (historyOfScores.includes(cityName)) {
+    getLatLong(searchInput);
+  } else {
+    historyOfScores.push(cityName);
+    localStorage.setItem('historyOfScores', JSON.stringify(historyOfScores));
+
+    searchHistoryButtonsCreation(cityName);
+
+    getLatLong(searchInput);
+  }
+})
+
+
+// Event listener for click on Search History buttons
+let searchBtns = document.querySelectorAll('#search-btn-history');
+for (let i = 0; i < searchBtns.length; i++) {
+  searchBtns[i].addEventListener('click', function () {
+
+    if (count > 0) {
+      document.querySelector('#target-container').remove()
+    }
+
+    console.log(searchBtns[i].value);
+
+    cityName = searchBtns[i].value;
+  
+
+    if (historyOfScores.includes(cityName)) {
+      getLatLong(cityName);
+    } else {
+      historyOfScores.push(cityName);
+      localStorage.setItem('historyOfScores', JSON.stringify(historyOfScores));
+
+      getLatLong(cityName);
+    }
+  })
+}
